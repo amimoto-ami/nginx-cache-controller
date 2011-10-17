15e9b8beee480a18d315f5ac81fdbd2147e8c718
Takayuki Miyauchi <miya@theta.ne.jp>
2011-10-18 01:44:19 +0900

    first commit

---
+++ nginx-champuru.php
@@ -0,0 +1,86 @@
+<?php
+/*
+Plugin Name: Nginx Champuru
+Author: Takayuki Miyauchi
+Plugin URI: http://firegoby.theta.ne.jp/wp/nginx-champuru
+Description: Plugin for Nginx Reverse Proxy 
+Version: 0.1.0
+Author URI: http://firegoby.theta.ne.jp/
+Domain Path: /languages
+Text Domain: nginx-champuru
+*/
+
+require_once(dirname(__FILE__).'/includes/class-addrewriterules.php');
+
+new NginxChampuru();
+register_activation_hook(__FILE__, 'flush_rewrite_rules');
+
+class NginxChampuru {
+
+private $js_version = "0.1.0";
+private $query = "nginxchampuru";
+
+function __construct()
+{
+    new WP_AddRewriteRules(
+        'nginx-champuru.json$',
+        $this->query,
+        array(&$this, 'get_commenter_json')
+    );
+    add_action("wp", array(&$this, "wp"));
+    add_action(
+        'wp_enqueue_scripts',
+        array(&$this, 'wp_enqueue_scripts')
+    );
+    add_filter(
+        "wp_get_current_commenter",
+        array(&$this, "wp_get_current_commenter")
+    );
+}
+
+public function wp()
+{
+    global $post;
+    if (post_password_required($post->ID)) {
+        nocache_headers();
+    }
+}
+
+public function wp_enqueue_scripts()
+{
+    if (!comments_open()) {
+        return;
+    }
+    wp_enqueue_script(
+        'nginx-champuru',
+        plugins_url('/nginx-champuru.js', __FILE__),
+        array('jquery'),
+        $this->js_version,
+        true
+    );
+}
+
+public function get_commenter_json()
+{
+    nocache_headers();
+    header('Content-type: application/json');
+    echo json_encode(wp_get_current_commenter());
+    exit;
+}
+
+public function wp_get_current_commenter($commenter)
+{
+    if (get_query_var($this->query)) {
+        return $commenter;
+    } else {
+        return array(
+            'comment_author'       => '',
+            'comment_author_email' => '',
+            'comment_author_url'   => '',
+        );
+    }
+}
+
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,47 @@
+=== Nginx Champuru ===
+Contributors: miyauchi
+Donate link: http://firegoby.theta.ne.jp/
+Tags: widget 
+Requires at least: 3.2
+Tested up to: 3.2
+Stable tag: 0.1.0
+
+Fix some cache problems on proxy server.
+
+== Description ==
+
+Fix some cache problems on proxy server.
+
+[This plugin maintained on GitHub.](https://github.com/miya0001/nginx-champuru)
+
+= Some features: =
+
+* Comments display author and email and url by ajax.
+* json data for ajax send no-cache header.
+* send no-cache header on password protected posts.
+
+= Contributors =
+
+* [Takayuki Miyauchi](http://firegoby.theta.ne.jp/)
+
+== Installation ==
+
+* A plug-in installation screen is displayed on the WordPress admin panel.
+* It installs it in `wp-content/plugins`.
+* The plug-in is made effective.
+
+== Changelog ==
+
+= 0.1.0 =
+* The first release.
+
+== Credits ==
+
+This plug-in is not guaranteed though the user of WordPress can freely use this plug-in free of charge regardless of the purpose.
+The author must acknowledge the thing that the operation guarantee and the support in this plug-in use are not done at all beforehand.
+
+== Contact ==
+
+* @miya0001 on twitter.
+* http://www.facebook.com/firegoby
+* https://github.com/miya0001/nginx-champuru
