5086a12480e8fbae81b1d50663f991a6abe1308e
miya <miya@wpist.me>
2013-05-10 05:18:25 +0900

    ver 1.5.0

--- nginx-champuru.php
+++ nginx-champuru.php
@@ -4,7 +4,7 @@ Plugin Name: Nginx Cache Controller
 Author: Ninjax Team (Takayuki Miyauchi)
 Plugin URI: http://ninjax.cc/
 Description: Plugin for Nginx Reverse Proxy
-Version: 1.4.2
+Version: 1.5.0
 Author URI: http://ninjax.cc/
 Domain Path: /languages
 Text Domain: nginxchampuru
