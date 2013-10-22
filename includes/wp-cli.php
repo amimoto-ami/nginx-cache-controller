c58ea584f011a0f9761c9c7044d6db2027632ba4
miya <miya@wpist.me>
2013-10-23 05:16:28 +0900

    bug fix

--- includes/wp-cli.php
+++ includes/wp-cli.php
@@ -4,32 +4,26 @@
  */
 class Nginx_Cache_Controller_Commands extends WP_CLI_Command {
 
-	/**
-	 * Flush proxy caches.
-	 *
-	 * ## OPTIONS
-	 *
-	 * [--url=<url>]
-	 * : The name of the person to greet.
-	 *
-	 * ## EXAMPLES
-	 *
-	 *     wp nginx flush
-	 *
-	 * @subcommand flush
-	 */
-	function flush($args, $assoc_args) {
-		global $nginxchampuru;
+    /**
+     * Flush proxy caches.
+     *
+     * ## OPTIONS
+     *
+     * [--url=<url>]
+     * : The name of the person to greet.
+     *
+     * ## EXAMPLES
+     *
+     *     wp nginx flush
+     *
+     * @subcommand flush
+     */
+    function flush($args, $assoc_args) {
+        global $nginxchampuru;
 
-		list($subcommand) = $args;
-
-		switch ($subcommand) {
-			case "flush":
-				$nginxchampuru->transientExec("flush_cache", 'all', 0);
-				WP_CLI::success( "All proxy caches are flushed." );
-				break;
-		}
-	}
+        $nginxchampuru->transientExec("flush_cache", 'all', 0);
+        WP_CLI::success( "All proxy caches are flushed." );
+    }
 }
 
 WP_CLI::add_command('nginx', 'Nginx_Cache_Controller_Commands');
