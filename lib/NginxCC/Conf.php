c0c97d8c401fa21561f0a090ec0a1b035dc3796a
miya0001 <miya@wpist.me>
2014-11-08 12:18:05 +0900

    Add constants to \NginxCC\Const

--- lib/NginxCC/Conf.php
+++ lib/NginxCC/Conf.php
@@ -4,6 +4,8 @@ namespace NginxCC;
 
 class Conf {
 
-    const cache_dir = '/var/cache/nginx';
-
+    const expire            = 86400;
+    const cache_dir         = "/var/cache/nginx";
+    const cache_levels      = "1:2";
+    const transient_timeout = 60;
 }
